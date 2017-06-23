a35e65433fc08385fa9765ee1a2d07b0c819e6c7
Ryan Davis <me@rdavis.xyz>
2017-06-23 19:41:04 +1000

    Ensure async map preserves ordering

--- hn/Extensions/Sequence.swift
+++ hn/Extensions/Sequence.swift
@@ -2,47 +2,47 @@ import Foundation
 
 extension Sequence {
     func map<U>(async perform: (Self.Iterator.Element, @escaping (U) -> Void) -> Void, withResult: @escaping ([U]) -> Void) {
-        var values = [U]()
+        var values = [(Int, U)]()
         let requestGroup = DispatchGroup()
-        forEach { value in
+        enumerated().forEach { index, value in
             requestGroup.enter()
             perform(value) { asyncValue in
-                values.append(asyncValue)
+                values.append((index, asyncValue))
                 requestGroup.leave()
             }
         }
         requestGroup.notify(queue: .main) {
-            withResult(values)
+            withResult(values.sorted(by: { $0.0 < $1.0 }).map { $0.1 })
         }
     }
 
     func flatMap<U>(async perform: (Self.Iterator.Element, @escaping (U?) -> Void) -> Void, withResult: @escaping ([U]) -> Void) {
-        var values = [U]()
+        var values = [(Int, U?)]()
         let requestGroup = DispatchGroup()
-        forEach { value in
+        enumerated().forEach { index, value in
             requestGroup.enter()
             perform(value) { asyncValue in
-                asyncValue.map { values.append($0) }
+                values.append((index, asyncValue))
                 requestGroup.leave()
             }
         }
         requestGroup.notify(queue: .main) {
-            withResult(values)
+            withResult(values.sorted(by: { $0.0 < $1.0 }).flatMap { $0.1 })
         }
     }
 
     func flatMap<U>(async perform: (Self.Iterator.Element, @escaping ([U]) -> Void) -> Void, withResult: @escaping ([U]) -> Void) {
-        var values = [U]()
+        var values = [(Int, [U])]()
         let requestGroup = DispatchGroup()
-        forEach { value in
+        enumerated().forEach { index, value in
             requestGroup.enter()
             perform(value) { asyncValue in
-                values += asyncValue
+                values.append((index, asyncValue))
                 requestGroup.leave()
             }
         }
         requestGroup.notify(queue: .main) {
-            withResult(values)
+            withResult(values.sorted(by: { $0.0 < $1.0 }).flatMap { $0.1 })
         }
     }
 }
