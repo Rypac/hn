286c6cdd01b84d0fb8c0411cc116e0575c956b93
Ryan Davis <me@rdavis.xyz>
2017-07-12 21:40:33 +1000

    Add url extension to extract hostname without www subdomain

---
+++ hnTests/Extensions/URL.swift
@@ -0,0 +1,29 @@
+@testable import hn
+import Quick
+import Nimble
+
+class URLSpec: QuickSpec {
+    override func spec() {
+        describe(".prettyHost") {
+            it("returns only the host when the url contains no subdomain") {
+                let url = URL(string: "https://example.com")
+                expect(url?.prettyHost).to(equal("example.com"))
+            }
+
+            it("returns only the host and subdomain when the url contains a non www subdomain") {
+                let url = URL(string: "https://this.example.io")
+                expect(url?.prettyHost).to(equal("this.example.io"))
+            }
+
+            it("returns only the host when the url contains a www subdomain") {
+                let url = URL(string: "https://www.example.com.au")
+                expect(url?.prettyHost).to(equal("example.com.au"))
+            }
+
+            it("returns only the host when the url contains additional path components") {
+                let url = URL(string: "http://example.org/stuff/here")
+                expect(url?.prettyHost).to(equal("example.org"))
+            }
+        }
+    }
+}
